2e922036a24b10b374063ad2793b080f9e38d347
pepedoa <58743526+pepedoa@users.noreply.github.com>
2026-01-12 13:29:40 +0100

    modificar formulario con fromspree

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -14,15 +14,15 @@ export class About implements AfterViewInit {
 
     const observer = new IntersectionObserver((entries) => {//creo esta funcion para que el observ amedida que detecta los items ejecute esto
       entries.forEach(entry => {
-        if (entry.isIntersecting) {
+        if (entry.isIntersecting) {//si el item está visible
           entry.target.classList.add('visible'); // aparece al bajar
         } else {
           entry.target.classList.remove('visible'); // desaparece al subir
         }
       });
-    }, { threshold: 0.5
+    }, { threshold: 0.5// 50% visible para activar
       
-     }); // 20% visible para activar
+     });
 
     items.forEach(item => observer.observe(item));//le digo al boserv estate atento a estos ítems, y una vez te aparezcan, ves haciéndolo de antes.
   }
--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +6,70 @@ import { Component } from '@angular/core';
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {
+export class Contact implements AfterViewInit{
+
+  ngAfterViewInit() {//se ejecuta despues de  que se haya  cargado;
+    const MAX_LENGTH = 300;
+    const textarea = document.getElementById('mensaje');
+    const contador = document.getElementById('contador');
+
+    if (textarea instanceof HTMLTextAreaElement  && contador instanceof HTMLElement ) {//verifico que sea un textarea
+
+      textarea.addEventListener("input", () => {
+        if (textarea.value.length > MAX_LENGTH) {
+          textarea.value = textarea.value.slice(0, MAX_LENGTH);
+        }
+
+        const longitud = textarea.value.length;
+        contador.textContent = `${longitud} / 300 caracteres`;
+      });
+    }
+
+
+  }
+
+
+  private endpoint = 'https://formspree.io/f/xojjqjpz'; // Pon tu ID real aquí
+
+  enviarFormulario(event: Event) {
+  event.preventDefault(); // Evita el envío por defecto del formulario
+
+  const form = event.target as HTMLFormElement;//obtengo el formulario
+  
+  const datos = {
+    name: (form.querySelector('#nombre') as HTMLInputElement).value,
+    email: (form.querySelector('#email') as HTMLInputElement).value,
+    message: (form.querySelector('#mensaje') as HTMLTextAreaElement).value
+  };
+
+  fetch(this.endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  })
+  .then(async response => {
+    // Usamos async/await dentro del then para que sea más fácil de leer
+    if (response.ok) {
+      console.log('¡Éxito!');
+      form.reset(); 
+      alert('Mensaje enviado correctamente');
+    } else {
+      const errorData = await response.json();
+      console.error('Error de Formspree:', errorData);
+      alert('Hubo un error al enviar el formulario.');
+    }
+  })
+  .catch(error => {
+    console.error('Error de red:', error);
+    alert('No se pudo conectar con el servidor.');
+  });
+}
+
+
+
+
 
 }
