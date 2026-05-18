1038b928e32f8f84749896305e6da29a6eb1d4d8
pepedoa <58743526+pepedoa@users.noreply.github.com>
2026-05-18 10:32:28 +0200

    Actualización del portfolio: nuevos proyectos y ajustes de estética

--- src/app/services/global-data.ts
+++ src/app/services/global-data.ts
@@ -10,6 +10,7 @@ export class GlobalData {
   private darkMode = new BehaviorSubject<boolean>(false);//creo variable y por defecto la pongo en false (modo claro), pero se ira actualizando si le doy al boton del tema
   darkMode$ = this.darkMode.asObservable();//podra ser utilizada por otros componente/servicios si se suscriben en su .ts
 
+  
   toggleDarkMode() {
     const newValue = !this.darkMode.value;//comprueba el valor actual y lo inviernte: si era false pasa a true, y viceversa.  para poder ir cambiando el tema 
     this.darkMode.next(newValue);//actualiza el valor, y asi  la comprobacion funcionara 
